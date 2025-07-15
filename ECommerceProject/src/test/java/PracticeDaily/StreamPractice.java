2ad40886dabdd4f4c8192b4ce2dc806e0fc910de
Kishor Amale <74699725+Ak-7798@users.noreply.github.com>
2025-07-15 11:47:07 -0600

    Update StreamPractice.java

--- ECommerceProject/src/test/java/PracticeDaily/StreamPractice.java
+++ ECommerceProject/src/test/java/PracticeDaily/StreamPractice.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class StreamPractice {
 	public static void main(String[] args) {
 		
-		
+		//added line for merge conflict
 		//Stream.of("Kishor","Don","Abhishek","Atul").filter(s->s.startsWith("A")).forEach(s->System.out.println(s));
 	}
 
